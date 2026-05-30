9d28d86527fec8303a67910be8fbf5f2f46cae73
Mohammed El Marouani <mohamed.elmarouani15@gmail.com>
2026-05-30 14:19:29 +0200

    debug: fond couleur + meshBasicMaterial (sans lumières)

--- src/components/escape3d/RiadScene.tsx
+++ src/components/escape3d/RiadScene.tsx
@@ -1,17 +1,36 @@
 "use client";
 
 import { Canvas } from "@react-three/fiber";
-import * as THREE from "three";
-import Courtyard from "./Courtyard";
 
 export default function RiadScene() {
   return (
     <div style={{ position: "absolute", inset: 0 }}>
       <Canvas
-        camera={{ position: [0, 4, 6], fov: 60, near: 0.1, far: 80 }}
+        camera={{ position: [0, 3, 8], fov: 75 }}
         style={{ width: "100%", height: "100%" }}
       >
-        <Courtyard />
+        {/* Fond rouge pour confirmer que le canvas rend bien */}
+        <color attach="background" args={["#220000"]} />
+
+        <ambientLight intensity={2} />
+
+        {/* Sol large et brillant */}
+        <mesh rotation={[-Math.PI / 2, 0, 0]} position={[0, 0, 0]}>
+          <planeGeometry args={[20, 20]} />
+          <meshBasicMaterial color="#1A5C3A" />
+        </mesh>
+
+        {/* Mur face caméra */}
+        <mesh position={[0, 1.75, -3.5]}>
+          <boxGeometry args={[7, 3.5, 0.2]} />
+          <meshBasicMaterial color="#F0EAD8" />
+        </mesh>
+
+        {/* Cube test au centre */}
+        <mesh position={[0, 1, 0]}>
+          <boxGeometry args={[1, 1, 1]} />
+          <meshBasicMaterial color="#D4AF37" />
+        </mesh>
       </Canvas>
     </div>
   );
